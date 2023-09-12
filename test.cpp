e98b0c74011c7528e4e1e3615195a7a7058c37ce
Dex9999 <95934184+Dex9999@users.noreply.github.com>
2023-09-12 10:02:24 -0400

    e

--- test.cpp
+++ test.cpp
@@ -5,9 +5,9 @@ int main(){
   int age = 16;
   char middleNameInitial = 'C';
   bool takingCompSci = true;
-  double income = 100000;
+  double income = 100000.75;
   float pi = 3.141592;
 
   std::cout << "Hello I'm " << name << " and I am " << age << " years old.\n"<< "My middle inital is " << middleNameInitial << "\nI am " << takingCompSci << "00% taking CompSci in University\nI hope to make $" << income << " per year ;) \nAlso pi is " << pi << "\n";
   return 0;
-  }
+}
