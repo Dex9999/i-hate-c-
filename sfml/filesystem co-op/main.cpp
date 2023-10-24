95c42b7548a8d33c212cd7629cdb16bbd03450fb
Dex9999 <95934184+Dex9999@users.noreply.github.com>
2023-10-24 11:41:20 -0400

    FOLDER WORKING!

--- sfml/filesystem co-op/main.cpp
+++ sfml/filesystem co-op/main.cpp
@@ -13,10 +13,13 @@ int main()
         // Handle the error if the image can't be loaded
         return EXIT_FAILURE;
     }
+    sf::Texture ftexture;
+    if (!ftexture.loadFromFile("folder.png")) {
+        // Handle the error if the image can't be loaded
+        return EXIT_FAILURE;
+    }
 
-    sf::RectangleShape button(sf::Vector2f(215, 50));
-    button.setPosition(100, 150);
-    button.setFillColor(sf::Color::Green);
+
 
     sf::Font font;
     if (!font.loadFromFile("font.ttf")) {
@@ -25,34 +28,33 @@ int main()
     }
 
     // Create button text
-    sf::Text buttonText("Create File", font, 24);
+    sf::Text buttonText("+", font, 24);
     buttonText.setPosition(120, 160);
     buttonText.setFillColor(sf::Color::White);
+
+    sf::RectangleShape button(sf::Vector2f(buttonText.getGlobalBounds().width*2, buttonText.getGlobalBounds().height*2));
+    button.setPosition(buttonText.getPosition().x-(buttonText.getGlobalBounds().width/2), buttonText.getPosition().y-(buttonText.getGlobalBounds().height/3));
+    button.setFillColor(sf::Color::Green);
 
     std::filesystem::path sourceFile("file.txt");
     std::filesystem::path destinationFolder("test");
 
-    if (std::filesystem::exists(sourceFile)) {
-        try {
-            std::filesystem::create_directory(destinationFolder);
-            std::filesystem::rename(sourceFile, destinationFolder / sourceFile.filename());
-            std::cout << "File moved successfully." << std::endl;
-        } catch (const std::filesystem::filesystem_error& e) {
-            std::cout << "Error: " << e.what() << std::endl;
-            // Handle the error or display a message to the user
-        }
-    } else {
-        std::cout << "Source file does not exist." << std::endl;
-        // Handle the error or display a message to the user
-    }
-
     // Create a sprite and set its texture
-    sf::Sprite sprite;
-    sprite.setTexture(texture);
-    sprite.scale(0.10f,0.10f);
-    sprite.setPosition(window.getSize().x/2,window.getSize().y/2);
+    sf::Sprite doc;
+    doc.setTexture(texture);
+    doc.scale(0.10f,0.10f);
+    doc.setPosition(window.getSize().x/2,window.getSize().y/2);
+
+    sf::Sprite folder;
+    folder.setTexture(ftexture);
+    folder.scale(0.10f,0.10f);
+    folder.setPosition(window.getSize().x/2+200,window.getSize().y/2);
 
     bool clicked = false;
+    bool docDraw = true;
+    // for dragging
+    bool isDragging = false;
+    sf::Vector2f offset;
 
 	// Start the game loop
     while (window.isOpen())
@@ -65,14 +67,16 @@ int main()
             if (event.type == sf::Event::Closed)
                 window.close();
         }
+        sf::Vector2i mousePosition = sf::Mouse::getPosition(window);
+        sf::FloatRect buttonBounds = button.getGlobalBounds();
+        sf::FloatRect docBounds = doc.getGlobalBounds();
+        sf::FloatRect folderBounds = folder.getGlobalBounds();
 
         if (event.type == sf::Event::MouseButtonPressed) {
-                // Check if the mouse click is within the button's bounds
                 if (event.mouseButton.button == sf::Mouse::Left) {
-                    sf::Vector2i mousePosition = sf::Mouse::getPosition(window);
-                    sf::FloatRect buttonBounds = button.getGlobalBounds();
 
-                    if (buttonBounds.contains(static_cast<sf::Vector2f>(mousePosition)) && clicked == false) {
+
+                    if (buttonBounds.contains(static_cast<sf::Vector2f>(mousePosition)) && !clicked) {
                         // Button clicked
                         std::ofstream file("file.txt");
                         if (file.is_open()) {
@@ -82,17 +86,60 @@ int main()
                             clicked = true;
                             button.setFillColor(sf::Color::Red);
                         } else {
-                            std::cerr << "Failed to create the file." << std::endl;
+                            std::cout << "Failed to create the file.";
                         }
                     }
+
+                    if (docBounds.contains(static_cast<sf::Vector2f>(mousePosition))) {
+                        // Mouse is pressed on the document; start dragging
+                        isDragging = true;
+                        offset = doc.getPosition() - static_cast<sf::Vector2f>(mousePosition);
+                    }
                 }
+
+            } else if (event.type == sf::Event::MouseButtonReleased) {
+                    if (event.mouseButton.button == sf::Mouse::Left) {
+                        isDragging = false;
+                    }
+                }
+
+            if (event.type == sf::Event::MouseMoved) {
+                    if (isDragging) {
+                        // Mouse is being dragged; move the document
+                        sf::Vector2i mousePosition = sf::Mouse::getPosition(window);
+                        doc.setPosition(static_cast<sf::Vector2f>(mousePosition) + offset);
+                    }
+            } else if (folderBounds.intersects(docBounds)) {
+                std::cout << "Moved file!";
+
+                if (std::filesystem::exists(sourceFile)) {
+                    try {
+                        std::filesystem::create_directory(destinationFolder);
+                        std::filesystem::rename(sourceFile, destinationFolder / sourceFile.filename());
+                        std::cout << "File moved successfully." << std::endl;
+                    } catch (const std::filesystem::filesystem_error& e) {
+                        std::cout << "Error: " << e.what() << std::endl;
+                        // Handle the error or display a message to the user
+                    }
+                } else {
+                    std::cout << "Source file does not exist." << std::endl;
+                    // Handle the error or display a message to the user
+                }
+
+                docDraw = false;
+                doc.setPosition(window.getSize().x / 2, window.getSize().y / 2);
+            } else if (docBounds.contains(static_cast<sf::Vector2f>(mousePosition)) && !clicked) {
+                doc.setPosition(window.getSize().x / 2, window.getSize().y / 2);
             }
 
         // Clear screen
         window.clear();
 
         // Draw the sprite
-        window.draw(sprite);
+        if(docDraw)
+            window.draw(doc);
+
+        window.draw(folder);
         window.draw(button);
         window.draw(buttonText);
 
