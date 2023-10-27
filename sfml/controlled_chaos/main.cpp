f8b9365b60eeb6745ad3d685abf1f6865f6d9ef5
Dex9999 <95934184+Dex9999@users.noreply.github.com>
2023-10-27 11:33:45 -0400

    starting controller input tester

--- sfml/controlled_chaos/main.cpp
+++ sfml/controlled_chaos/main.cpp
@@ -5,6 +5,21 @@ int main()
 {
     sf::RenderWindow window(sf::VideoMode(800, 600), "Reeeee");
 
+    //sfml only supports up to 8 controllers
+    for(int i = 0; i < 8; ++i){
+        //if(sf::Joystick::isConnected(i))
+        std::cout << "Controller " << i+1 << ": " << (sf::Joystick::isConnected(i) ? "connected": "not connected") << "\n";
+        std::cout << (sf::Joystick::getButtonCount(i) == 0 ? "" : std::to_string(sf::Joystick::getButtonCount(i))) << (sf::Joystick::getButtonCount(i) == 0 ? "" : " buttons are detected.\n");
+        std::cout << (sf::Joystick::hasAxis(i, sf::Joystick::X) ? "X, " : "") << (sf::Joystick::hasAxis(i, sf::Joystick::Y) ? "Y, " : "") << (sf::Joystick::hasAxis(i, sf::Joystick::Z) ? "Z, " : "");
+        std::cout << (sf::Joystick::hasAxis(i, sf::Joystick::R) ? "R, " : "") << (sf::Joystick::hasAxis(i, sf::Joystick::U) ? "U, " : "") << (sf::Joystick::hasAxis(i, sf::Joystick::V) ? "V, " : "");
+        std::cout << " axes found\n";
+    }
+    // Is button #2 pressed on joystick #0?
+    bool pressed = sf::Joystick::isButtonPressed(0, 2);
+
+    // What's the current position of the Y axis on joystick #0?
+    float position = sf::Joystick::getAxisPosition(0, sf::Joystick::Y);
+
     while (window.isOpen())
     {
         sf::Event event;
