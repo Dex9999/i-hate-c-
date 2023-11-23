042582ac871cccbce9776dea4935791a6d673108
Dex9999 <95934184+Dex9999@users.noreply.github.com>
2023-11-23 10:29:39 -0500

    e

--- sfml/space-invaders/main.cpp
+++ sfml/space-invaders/main.cpp
@@ -1,5 +1,5 @@
 #include "SFML/Graphics.hpp"
-
+#include <vector>
 int main(){
   sf::RenderWindow window(sf::VideoMode(600, 650), "Sprites!");
   sf::Clock clock;
@@ -8,10 +8,8 @@ int main(){
   sf::Texture texture;
   texture.loadFromFile("spritesheet.png");
 
-  //sf::Sprite full(texture,sf::IntRect(0,0,110,8));
-  //full.scale(5,5);
-
-  sf::IntRect textureRectArray[55];
+  std::vector<sf::IntRect> textureRectArray;
+  std::vector<sf::Sprite> enemyArray;
 
   sf::Sprite greenNeutral(texture,sf::IntRect(0,0,8,8));
   sf::Sprite greenOpen(texture,sf::IntRect(9,0,8,8));
@@ -41,17 +39,15 @@ int main(){
   greenOpen.scale(5,5);
   greenDeath.scale(5,5);
 
-  sf::Sprite enemyArray[55];
-
   int currEnemy = 0;
   sf::IntRect tex;
 
   for(int i = 0; i<11; ++i){
     for(int j = 0; j<5; ++j){
-        currEnemy = i+j*11;
+        sf::Sprite indEnemy;
 
-        enemyArray[currEnemy].setTexture(texture);
-        enemyArray[currEnemy].setScale(2,2);
+        indEnemy.setTexture(texture);
+        indEnemy.setScale(2,2);
         switch(j){
             case 0:
             tex = sf::IntRect(0,0,8,8);
@@ -70,9 +66,10 @@ int main(){
             tex = sf::IntRect(0,72,8,8);
               break;
         }
-        textureRectArray[currEnemy] = tex;
-        enemyArray[currEnemy].setTextureRect(tex);
-        enemyArray[currEnemy].setPosition(i*20+100, j*20);
+        textureRectArray.push_back(tex);
+        indEnemy.setTextureRect(tex);
+        indEnemy.setPosition(i*20+100, j*20);
+        enemyArray.push_back(indEnemy);
     }
   }
 
@@ -82,27 +79,38 @@ int main(){
   int seconds = 0;
   sf::Vector2f velocity(0.25,0);
 
+  bool bounce = false;
+
 
   while (window.isOpen()){
     while (window.pollEvent(event)){
       if (event.type == sf::Event::EventType::Closed)
         window.close();
     }
-    elapsed = clock.getElapsedTime();
 
-    //floor it
-    seconds = elapsed.asSeconds();
-    if(enemyArray[55].getPosition().x > window.getSize().x || enemyArray[0].getPosition().x < 0)
-    {
-            velocity.x = -velocity.x;
-            //velocity.y = velocity.y * 2;
-    }
-    for(int i = 0; i < 55; ++i){
-        enemyArray[i].setPosition(enemyArray[i].getPosition().x+velocity.x,enemyArray[i].getPosition().y+velocity.y);
-    }
-    //if(velocity.y > 2){velocity.y = velocity.y/2;}
+
+    for(int i = 0; i < enemyArray.size(); ++i){
+        enemyArray[i].move(velocity.x,velocity.y);
+
+        if(enemyArray[i].getPosition().x > window.getSize().x-15 || enemyArray[i].getPosition().x < 0){
+            bounce = 1;
+        }
+
+        if(bounce){
+            bounce = 0;
+
+            velocity.x *= -1.01;
+            for(auto& i: enemyArray){
+                i.move(0,15);
+            }
+        }
+     }
+
     window.clear();
 
+    elapsed = clock.getElapsedTime();
+    // force int conversion to floor
+    seconds = elapsed.asSeconds();
     if (seconds == 0){
         window.draw(blueNeutral);
     } else if(seconds == 1){
@@ -113,7 +121,9 @@ int main(){
     if(seconds == 3){
         clock.restart();
     }
-    for(int i = 0; i < 55; ++i){
+
+    for(int i = 0; i < enemyArray.size(); ++i){
+        enemyArray[i].setTextureRect(textureRectArray[i]);
         window.draw(enemyArray[i]);
     }
 
